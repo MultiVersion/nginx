b7c7f36a82a802c04c07cdcc0f695c6c3df47fbf
Dviih <dviih@dviih.email>
2021-09-02 13:47:48 -0300

    PHP-FPM Tests

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,11 +5,16 @@ if [[ ! -f nginx.conf ]]; then
   cp /opt/nginx.conf /home/container/nginx.conf
 fi
 
+if [[ ! -f php-fpm.conf ]]; then
+  cp /opt/php-fpm.conf /home/container/php-fpm.conf
+fi
+
 if [[ ! -d /home/container/logs ]]; then
   mkdir -p /home/container/logs
 fi
 
 sed -i "s/changemyport/$SERVER_PORT/g" /home/container/nginx.conf
 
+php-fpm8.0 -p /home/container -y /home/container/php-fpm.conf -D
 nginx -g "daemon off;"
 exit 1
